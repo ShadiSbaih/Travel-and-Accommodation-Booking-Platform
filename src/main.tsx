22fbb090db2cd39fcb085200471d93ce6827f2e4
Shadi Sbaih <shadisbaih20014@gmail.com>
2025-10-11 13:10:32 +0300

    feat: update main rendering logic to include BrowserRouter and AppRoutes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
 import { ThemeProvider } from './context/Theme/ThemeProvider.tsx'
+import AppRoutes from './routes/AppRoutes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
 )
