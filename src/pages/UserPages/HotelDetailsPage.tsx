e7660fa761f10b4aae54f702025387eccc50a1c1
Shadi Sbaih <shadisbaih20014@gmail.com>
2025-10-13 18:16:31 +0300

    feat: Integrate Navbar component into HotelDetailsPage for improved navigation

--- src/pages/UserPages/HotelDetailsPage.tsx
+++ src/pages/UserPages/HotelDetailsPage.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Navbar from '@/components/Navbar'
 
 function HotelDetailsPage() {
   return (
+    <>
+      <Navbar />
     <div>HotelDetailsPage</div>
+    </>
   )
 }
 
